23b0d3c1b6af6234cfa42bd568d296a07ba7e2af
Larry Masullo <lmasullo@gmail.com>
2019-08-10 12:55:56 -0500

    Ugh!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,12 +6,15 @@ const mysql = require('mysql');
 // Require the inquirer npm package
 const inquirer = require('inquirer');
 
+let prodID = 0;
+let prodQuant = 0;
+
 // Create a connection
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'Laxman27',
+  password: 'password',
   database: 'bamazon',
 });
 
@@ -28,56 +31,59 @@ function readProducts() {
     if (err) throw err;
     // Log all results of the SELECT statement
     console.table(res);
-    //connection.end();
+    // connection.end();
 
     // Call the Initial Buy Inquirer
     inqBuy();
   });
 }
 
-function makePurchase(answers){
-  //First check if there is sufficient quantity
+function makePurchase(answers) {
+  // First check if there is sufficient quantity
   console.log('Checking Product Stock Level...\n');
-  let prodID = answers.id;
+  prodID = answers.id;
+  prodQuant = answers.units;
   console.log(prodID);
-  
-  let sql = 'SELECT stock_quantity FROM products Where item_id = ' + connection.escape(prodID);
+
+  const sql = `SELECT stock_quantity FROM products Where item_id = ${connection.escape(prodID)}`;
   connection.query(sql, (err, res) => {
     if (err) throw err;
     // Log all results of the SELECT statement
     console.log(res[0].stock_quantity);
-    let result = res[0].stock_quantity;
-    if (result > answers.units){
-      //Update quantity
-      let updatedQuant = result - answers.units;
-      //Make the purchase
-      console.log('Completing the Purchase...\n');
+    const result = res[0].stock_quantity;
+    if (result > prodQuant) {
+      // Update quantity
+      const updatedQuant = result - prodQuant;
 
-      
-        const query = connection.query(
-          'UPDATE products SET ? WHERE ?',
-          [
-            {
-              stock_quantity: updatedQuant,
-            },
-            {
-              item_id: answers.id,
-            },
-          ],
-          (err, res) => {
-            if (err) throw err;
-            console.log(`${res.affectedRows} products updated!\n`);
-            //todo Show updated quant
+      // Calculate Cost
+      const costResult = cost(prodID, prodQuant);
+      console.log(`Cost Result: ${costResult}`);
+
+      // Make the purchase
+      console.log(`Completing the Purchase...\nYour total cost is ${costResult}`);
+
+      const query = connection.query(
+        'UPDATE products SET ? WHERE ?',
+        [
+          {
+            stock_quantity: updatedQuant,
           },
-        );
-        
-
-    }else{
-      console.log('Stock Quantity Insufficient, unable to complete the purchase.\nTry again tomorrow, thank you.');
+          {
+            item_id: answers.id,
+          },
+        ],
+        (err, res) => {
+          if (err) throw err;
+          console.log(`${res.affectedRows} products updated!\n`);
+          // todo Show updated quant
+        },
+      );
+    } else {
+      console.log(
+        'Stock Quantity Insufficient, unable to complete the purchase.\nTry again tomorrow, thank you.',
+      );
     }
   });
-
-
 }
 
 function inqBuy() {
@@ -96,10 +102,29 @@ function inqBuy() {
     ])
     .then((answers) => {
       console.log(answers);
-      //Call makePurchase function, pass the answers object
+      // Call makePurchase function, pass the answers object
       makePurchase(answers);
     });
 }
 
+// Get the price
+function cost(prodID, prodQuant) {
+  const sql = `SELECT price FROM products Where item_id = ${connection.escape(prodID)}`;
+  connection.query(sql, (err, res) => {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
 
-//!Need to quit connection!!!!!
+    // console.log(res[0].price);
+    const price = res[0].price;
+
+    const calcCost = price * prodQuant;
+    // console.log(calcCost);
+
+    return calcCost;
+
+    // connection.end();
+  });
+}
+
+//! Need to quit connection!!!!!
